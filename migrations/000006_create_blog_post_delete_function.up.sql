ab73165fbb8d9585c25eded693a0c5b1c4913175
r3d5un <r3d5un@pm.me>
2025-08-13 01:33:33 +0200

    Delete non-functioning function and trigger
    PostgreSQL is not able to update a row about to be deleted through
    a BEFORE DELETE statement. Migrations therefore non functioning
    like planned and redundant.

--- migrations/000006_create_blog_post_delete_function.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPlACE FUNCTION delete_blog_post_deleted_at_timestamp()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    NEW.deleted = TRUE;
-    NEW.deleted_at = NOW();
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
--- migrations/000007_create_blog_post_delete_trigger.up.sql
+++
@@ -1,6 +0,0 @@
-DROP TRIGGER IF EXISTS trigger_blog_post_timestamp_on_delete ON blog.post;
-
-CREATE TRIGGER trigger_blog_post_timestamp_on_delete
-    BEFORE DELETE ON blog.post
-    FOR EACH ROW
-EXECUTE PROCEDURE delete_blog_post_deleted_at_timestamp();
