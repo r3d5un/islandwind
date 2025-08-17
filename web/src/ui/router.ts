c6a3c56b96c222414a0d8bbb8fd65f0a6e467fb0
r3d5un <r3d5un@pm.me>
2025-08-17 15:32:35 +0200

    Use HTML5 web history

--- web/src/ui/router.ts
+++ web/src/ui/router.ts
@@ -1,15 +1,16 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/components/HomeView.vue'
 import BlogpostView from '@/components/BlogpostView.vue'
 import NotFoundView from '@/components/NotFoundView.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
+  { path: '/blog', redirect: '/' },
   { path: '/blog/:id', name: 'BlogpostView', component: BlogpostView },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
