eb8d7d47d2420c3bbcfce82369226e63bbd6aa3d
r3d5un <r3d5un@pm.me>
2025-08-18 14:31:59 +0200

    Create refresh token database table

---
+++ migrations/000007_create_refresh_token_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS auth.refresh_token
+(
+    id         UUID DEFAULT gen_random_uuid(),
+    issuer     VARCHAR(512) NOT NULL,
+    audience   VARCHAR(512) NOT NULL,
+    expiration TIMESTAMP    NOT NULL,
+    issued_at  TIMESTAMP    NOT NULL,
+    not_before TIMESTAmP    NOT NULL,
+    CONSTRAINT pk_refresh_token_id PRIMARY KEY (id)
+);
