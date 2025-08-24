c1291599794da2ac08665428a074d3c383e82b86
Øyvind Kristiansen <r3d5un@pm.me>
2025-08-24 22:15:19 +0200

    Reimplement a blogpost API client

---
+++ web/src/api/blog.ts
@@ -0,0 +1,76 @@
+import { type AxiosInstance, type AxiosResponse } from 'axios'
+import {
+  Blogpost,
+  BlogpostInput,
+  BlogpostListResponse,
+  BlogpostPatch,
+  BlogpostPatchBody,
+  BlogpostPostBody,
+  type IBlogpostListResponse,
+  type IBlogpostResponse,
+} from '@/api/blogposts.ts'
+import { handleRequestFailure, type RequestFailureError } from '@/api/errors.ts'
+
+export class BlogpostApiClient {
+  private client: AxiosInstance
+  constructor(client: AxiosInstance) {
+    this.client = client
+  }
+
+  public async get(id: string): Promise<Blogpost | RequestFailureError> {
+    try {
+      const response: AxiosResponse<IBlogpostResponse, number> = await this.client.get(
+        `/api/v1/blog/post/${id}`,
+      )
+      return new Blogpost(response.data.data)
+    } catch (error) {
+      return handleRequestFailure(error)
+    }
+  }
+
+  public async list(): Promise<BlogpostListResponse | RequestFailureError> {
+    try {
+      const response: AxiosResponse<IBlogpostListResponse, number> =
+        await this.client.get('/api/v1/blog/post')
+      return new BlogpostListResponse(response.data.data, response.data.metadata)
+    } catch (error) {
+      return handleRequestFailure(error)
+    }
+  }
+
+  public async patch(blogpost: BlogpostPatch): Promise<Blogpost | RequestFailureError> {
+    try {
+      const response: AxiosResponse<IBlogpostResponse, number> = await this.client.patch(
+        '/api/v1/blog/post',
+        new BlogpostPatchBody(blogpost),
+      )
+      return new Blogpost(response.data.data)
+    } catch (error) {
+      return handleRequestFailure(error)
+    }
+  }
+
+  public async post(input: BlogpostInput): Promise<Blogpost | RequestFailureError> {
+    try {
+      const response: AxiosResponse<IBlogpostResponse, number> = await this.client.post(
+        '/api/v1/blog/post',
+        new BlogpostPostBody(input),
+      )
+      return new Blogpost(response.data.data)
+    } catch (error) {
+      return handleRequestFailure(error)
+    }
+  }
+
+  public async delete(id: string): Promise<Blogpost | RequestFailureError> {
+    try {
+      const response: AxiosResponse<IBlogpostResponse, number> = await this.client.request({
+        method: 'delete',
+        url: `/api/v1/blog/post/${id}`,
+      })
+      return new Blogpost(response.data.data)
+    } catch (error) {
+      return handleRequestFailure(error)
+    }
+  }
+}
--- web/src/ui/client.ts
+++ web/src/ui/client.ts
@@ -1,9 +1,78 @@
 import { useLogger } from './logging.ts'
 import { HttpClient } from '@/api/client.ts'
+import axios, {
+  AxiosError,
+  type AxiosInstance,
+  type AxiosResponse,
+  type InternalAxiosRequestConfig,
+} from 'axios'
+import { refresh, Tokens, useAuthStore } from '@/api/auth.ts'
+import { handleRequestFailure } from '@/api/errors.ts'
 
 const baseUrl: string = 'http://localhost:4000'
 
 const httpClient = new HttpClient(useLogger(), baseUrl)
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 5_000,
+})
+
+apiClient.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    let accessToken: string = ''
+    const authStore = useAuthStore()
+    if (authStore.loggedIn) {
+      accessToken = authStore.tokens.accessToken
+    }
+
+    if (accessToken) {
+      config.headers = config.headers || {}
+      config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error)
+  },
+)
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean
+}
+
+apiClient.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as CustomAxiosRequestConfig
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+
+      try {
+        const authStore = useAuthStore()
+        const newTokens = await refresh(authStore.tokens.refreshToken)
+
+        if (newTokens instanceof Tokens) {
+          authStore.tokens.accessToken = newTokens.accessToken
+          authStore.tokens.refreshToken = newTokens.refreshToken
+        }
+
+        originalRequest.headers.Authorization = `Bearer ${authStore.tokens.accessToken}`
+
+        return apiClient(originalRequest)
+      } catch (refreshError) {
+        return Promise.reject(handleRequestFailure(refreshError))
+      }
+    }
+
+    return Promise.reject(handleRequestFailure(error))
+  },
+)
+
+export function useApiClient() {
+  return apiClient
+}
 
 export function useClient() {
   return httpClient
