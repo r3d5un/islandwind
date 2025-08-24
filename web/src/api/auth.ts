29c2b27b2d351b852f3c554df2216ffe00372bf4
Øyvind Kristiansen <r3d5un@pm.me>
2025-08-24 02:10:32 +0200

    Store access and refresh tokens in an authentication store

--- web/src/api/auth.ts
+++ web/src/api/auth.ts
@@ -1,12 +1,17 @@
 import { handleRequestFailure, type RequestFailureError } from '@/api/errors.ts'
 import axios, { type AxiosResponse } from 'axios'
+import { defineStore, type StoreDefinition } from 'pinia'
+
+export const useAuthStore: StoreDefinition<'tokens', { tokens: Tokens }> = defineStore('tokens', {
+  state: () => ({ tokens: new Tokens({ accessToken: '', refreshToken: '' }) }),
+})
 
 export async function login(
   username: string,
   password: string,
 ): Promise<Tokens | RequestFailureError> {
   try {
-    const response: AxiosResponse<LoginResponse, number> = await axios({
+    const response: AxiosResponse<ITokens, number> = await axios({
       method: 'post',
       url: `${import.meta.env.VITE_API_URL}/api/v1/auth/login`,
       timeout: import.meta.env.VITE_API_TIMEOUT,
@@ -20,7 +25,7 @@ export async function login(
 
 export async function refresh(refreshToken: string): Promise<Tokens | RequestFailureError> {
   try {
-    const response: AxiosResponse<LoginResponse, number> = await axios.post(
+    const response: AxiosResponse<ITokens, number> = await axios.post(
       `${import.meta.env.VITE_API_URL}/api/v1/auth/refresh`,
       new RefreshRequestBody(refreshToken),
       { timeout: import.meta.env.VITE_API_TIMEOUT },
@@ -31,7 +36,7 @@ export async function refresh(refreshToken: string): Promise<Tokens | RequestFai
   }
 }
 
-export interface LoginResponse {
+export interface ITokens {
   accessToken: string
   refreshToken: string
 }
@@ -40,7 +45,7 @@ export class Tokens {
   public accessToken: string
   public refreshToken: string
 
-  constructor(input: LoginResponse) {
+  constructor(input: ITokens) {
     this.accessToken = input.accessToken
     this.refreshToken = input.refreshToken
   }
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { router } from './ui/router'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(router).use(pinia).mount('#app')
