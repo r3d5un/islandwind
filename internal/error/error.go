05b0d0ffbbcdca828ee148ee25e782989685b052
r3d5un <r3d5un@pm.me>
2026-04-01 21:36:30 +0200

    Create methods and functions to enable ergonomic error handling and logging
    Signed-off-by: r3d5un <r3d5un@pm.me>

--- internal/error/error.go
+++ internal/error/error.go
@@ -1,12 +1,43 @@
 package error
 
+import (
+	"errors"
+	"log/slog"
+)
+
 type Error struct {
-	Code     string `json:"code"`
-	Message  string `json:"message"`
-	Internal error
+	Code     string         `json:"code"`
+	Message  string         `json:"message"`
+	Internal error          `json:"internal"`
 	Metadata map[string]any `json:"metadata,omitempty"`
 }
 
+func New(code, message string, internal error, metadata map[string]any) *Error {
+	return &Error{
+		Code:     code,
+		Message:  message,
+		Internal: errors.Join(internal),
+		Metadata: metadata,
+	}
+}
+
+// Unwrap implements the error interface.
+func (e *Error) Unwrap() error {
+	return e.Internal
+}
+
+// Error implements the error interface.
 func (e *Error) Error() string {
 	return e.Message
+}
+
+// LogValue implements the slog.LogValuer interface. It returns a slog.Value containing the details
+// of the error for structured logging.
+func (e *Error) LogValue() slog.Value {
+	return slog.GroupValue(
+		slog.String("code", e.Code),
+		slog.String("message", e.Message),
+		slog.String("internal", e.Internal.Error()),
+		slog.Any("metadata", e.Metadata),
+	)
 }
