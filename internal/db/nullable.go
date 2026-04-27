07642d011a7923021da91cce9d57da4b88fc4f2d
r3d5un <r3d5un@pm.me>
2026-04-27 18:47:31 +0200

    Create an ExplicitNull interface for the database package
    Signed-off-by: r3d5un <r3d5un@pm.me>

--- internal/db/nullable.go
+++ internal/db/nullable.go
@@ -1,11 +1,20 @@
 package db
 
+import (
+	"github.com/r3d5un/islandwind/internal/nullable"
+)
+
+var (
+	_ ExplicitNull = nullable.Nullable[string]{}
+	_ ExplicitNull = (*nullable.Nullable[string])(nil)
+)
+
 // ExplicitNull represents a tri-state nullable value used in query filters.
 //
 // States:
-// - unspecified: filter should be ignored
-// - null: filter should target SQL NULL
-// - value: filter should apply with a concrete value
+// - Unspecified: filter should be ignored
+// - Null: filter should target SQL NULL
+// - Value: filter should apply with a concrete value
 type ExplicitNull interface {
 	IsSpecified() bool
 	IsNull() bool
