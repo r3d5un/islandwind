b9fcaf956f23ba43d95eae3247df0b772d4f6680
r3d5un <r3d5un@pm.me>
2026-05-09 20:44:36 +0200

    Create a ReadQueryNullInt32 function
    Signed-off-by: r3d5un <r3d5un@pm.me>

--- internal/api/api.go
+++ internal/api/api.go
@@ -241,6 +241,19 @@ func ReadRequiredQueryInt(qs url.Values, key string, defaultVal int, v *validato
 
 	return i
 }
+
+func ReadQueryNullInt32(qs url.Values, key string, v *validator.Validator) sql.NullInt64 {
+	s := qs.Get(key)
+	if s == "" {
+		return sql.NullInt64{Valid: false}
+	}
+	i, err := strconv.ParseInt(s, 10, 32)
+	if err != nil {
+		v.AddError(key, fmt.Sprintf("unable to parse value: %s", err.Error()))
+	}
+	return sql.NullInt64{Int64: i, Valid: true}
+}
+
 func ReadQueryNullInt64(qs url.Values, key string, v *validator.Validator) sql.NullInt64 {
 	s := qs.Get(key)
 	if s == "" {
