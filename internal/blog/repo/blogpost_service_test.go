6a6725ef0f15135f3e78c399cc09ee468a8d396b
r3d5un <r3d5un@pm.me>
2026-05-14 13:35:19 +0200

    Rewrite tests for the blogpost service

--- internal/blog/repo/blogpost_service_test.go
+++ internal/blog/repo/blogpost_service_test.go
@@ -5,20 +5,16 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/r3d5un/islandwind/internal/blog/data"
 	"github.com/r3d5un/islandwind/internal/blog/repo"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPostRepository(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
-	t.Cleanup(func() {
-		t.Logf("cleaning up test: %s", t.Name())
-		defer cancel()
-	})
-
-	var post repo.Post
+	defer cancel()
 
 	t.Run("Create", func(t *testing.T) {
 		created, err := blog.Posts.Create(
@@ -29,67 +25,145 @@ func TestPostRepository(t *testing.T) {
 				Published: true,
 			},
 		)
-		assert.NoError(t, err)
-		assert.NotNil(t, created)
+		require.NoError(t, err)
+		require.NotNil(t, created)
 
-		post = *created
+		t.Cleanup(func() {
+			require.NoError(t, blog.Posts.Purge(ctx, created.ID))
+		})
 	})
 
 	t.Run("Read", func(t *testing.T) {
-		read, err := blog.Posts.Read(ctx, post.ID)
-		assert.NoError(t, err)
-		assert.NotNil(t, read)
-		assert.Equal(t, post, *read)
+		created, err := blog.Posts.Create(
+			ctx,
+			repo.PostInput{
+				Title:     "Example Title",
+				Content:   "Some placeholder content",
+				Published: true,
+			},
+		)
+		require.NoError(t, err)
+
+		t.Cleanup(func() {
+			require.NoError(t, blog.Posts.Purge(ctx, created.ID))
+		})
+
+		read, err := blog.Posts.Read(ctx, created.ID)
+		require.NoError(t, err)
+		require.NotNil(t, read)
+		assert.Equal(t, *created, *read)
 	})
 
 	t.Run("List", func(t *testing.T) {
-		list, metadata, err := blog.Posts.List(
+		created, err := blog.Posts.Create(
 			ctx,
-			data.Filter{
-				PageSize: 1,
-				ID:       &post.ID,
+			repo.PostInput{
+				Title:     "Example Title",
+				Content:   "Some placeholder content",
+				Published: true,
 			},
 		)
-		assert.NoError(t, err)
-		assert.NotNil(t, list)
-		assert.NotEmpty(t, list)
+		require.NoError(t, err)
+
+		t.Cleanup(func() {
+			require.NoError(t, blog.Posts.Purge(ctx, created.ID))
+		})
+
+		list, metadata, err := blog.Posts.List(
+			ctx,
+			data.PostFilter{
+				PageSize: 1,
+				ID:       uuid.NullUUID{UUID: created.ID, Valid: true},
+			},
+		)
+		require.NoError(t, err)
+		require.NotNil(t, list)
+		require.NotEmpty(t, list)
 		assert.NotEmpty(t, metadata)
 		assert.Equal(t, list[len(list)-1].ID, metadata.LastSeen)
 	})
 
 	t.Run("Update", func(t *testing.T) {
+		created, err := blog.Posts.Create(
+			ctx,
+			repo.PostInput{
+				Title:     "Example Title",
+				Content:   "Some placeholder content",
+				Published: true,
+			},
+		)
+		require.NoError(t, err)
+
+		t.Cleanup(func() {
+			require.NoError(t, blog.Posts.Purge(ctx, created.ID))
+		})
+
 		updated, err := blog.Posts.Update(
 			ctx,
-			repo.PostPatch{ID: post.ID, Deleted: new(false)},
+			repo.PostPatch{ID: created.ID, Deleted: new(false)},
 		)
-		assert.NoError(t, err)
-		assert.NotNil(t, updated)
-		assert.Equal(t, post.ID, updated.ID)
+		require.NoError(t, err)
+		require.NotNil(t, updated)
+		assert.Equal(t, created.ID, updated.ID)
 		assert.Empty(t, updated.DeletedAt)
 		assert.False(t, updated.Deleted)
-
-		post = *updated
 	})
 
 	t.Run("Delete", func(t *testing.T) {
-		err := blog.Posts.Delete(ctx, post.ID)
-		assert.NoError(t, err)
+		created, err := blog.Posts.Create(
+			ctx,
+			repo.PostInput{
+				Title:     "Example Title",
+				Content:   "Some placeholder content",
+				Published: true,
+			},
+		)
+		require.NoError(t, err)
+
+		t.Cleanup(func() {
+			require.NoError(t, blog.Posts.Purge(ctx, created.ID))
+		})
+
+		require.NoError(t, blog.Posts.Delete(ctx, created.ID))
 	})
 
 	t.Run("Restore", func(t *testing.T) {
-		restored, err := blog.Posts.Restore(ctx, post.ID)
-		assert.NoError(t, err)
-		assert.NotEmpty(t, restored)
-		assert.Equal(t, post.ID, restored.ID)
+		created, err := blog.Posts.Create(
+			ctx,
+			repo.PostInput{
+				Title:     "Example Title",
+				Content:   "Some placeholder content",
+				Published: true,
+			},
+		)
+		require.NoError(t, err)
+
+		t.Cleanup(func() {
+			require.NoError(t, blog.Posts.Purge(ctx, created.ID))
+		})
+
+		require.NoError(t, blog.Posts.Delete(ctx, created.ID))
+
+		restored, err := blog.Posts.Restore(ctx, created.ID)
+		require.NoError(t, err)
+		require.NotEmpty(t, restored)
+		assert.Equal(t, created.ID, restored.ID)
 		assert.False(t, restored.Deleted)
 		assert.Nil(t, restored.DeletedAt)
-		assert.Equal(t, post.ID, restored.ID)
-
-		post = *restored
+		assert.Equal(t, created.ID, restored.ID)
 	})
 
 	t.Run("Purge", func(t *testing.T) {
-		err := blog.Posts.Purge(ctx, post.ID)
-		assert.NoError(t, err)
+		created, err := blog.Posts.Create(
+			ctx,
+			repo.PostInput{
+				Title:     "Example Title",
+				Content:   "Some placeholder content",
+				Published: true,
+			},
+		)
+		require.NoError(t, err)
+
+		require.NoError(t, blog.Posts.Purge(ctx, created.ID))
 	})
 }
