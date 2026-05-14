282d266d3df906b411159adf0c1fce045044a865
r3d5un <r3d5un@pm.me>
2026-05-14 13:45:56 +0200

    Rewrite tests for the blogpost data model

--- internal/blog/data/post_test.go
+++ internal/blog/data/post_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/r3d5un/islandwind/internal/blog/data"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBlogModel(t *testing.T) {
@@ -18,59 +19,101 @@ func TestBlogModel(t *testing.T) {
 		defer cancel()
 	})
 
-	var post data.Post
-
 	t.Run("Insert", func(t *testing.T) {
 		inserted, err := models.Posts.Insert(ctx, data.PostInput{
 			Title:     "Test",
 			Content:   "Some example content",
 			Published: true,
 		})
-		assert.NoError(t, err)
-		assert.NotNil(t, inserted)
+		require.NoError(t, err)
+		require.NotNil(t, inserted)
 
-		post = *inserted
+		t.Cleanup(func() {
+			_, err := models.Posts.Delete(ctx, inserted.ID)
+			require.NoError(t, err)
+		})
 	})
 
 	t.Run("Select", func(t *testing.T) {
-		selected, err := models.Posts.SelectOne(ctx, post.ID)
-		assert.NoError(t, err)
-		assert.NotNil(t, selected)
-		assert.Equal(t, post, *selected)
+		inserted, err := models.Posts.Insert(ctx, data.PostInput{
+			Title:     "Test",
+			Content:   "Some example content",
+			Published: true,
+		})
+		require.NoError(t, err)
+		require.NotNil(t, inserted)
+
+		t.Cleanup(func() {
+			_, err := models.Posts.Delete(ctx, inserted.ID)
+			require.NoError(t, err)
+		})
+
+		selected, err := models.Posts.SelectOne(ctx, inserted.ID)
+		require.NoError(t, err)
+		require.NotNil(t, selected)
+		assert.Equal(t, *inserted, *selected)
 	})
 
 	t.Run("SelectMany", func(t *testing.T) {
+		inserted, err := models.Posts.Insert(ctx, data.PostInput{
+			Title:     "Test",
+			Content:   "Some example content",
+			Published: true,
+		})
+		require.NoError(t, err)
+		require.NotNil(t, inserted)
+
+		t.Cleanup(func() {
+			_, err := models.Posts.Delete(ctx, inserted.ID)
+			require.NoError(t, err)
+		})
+
 		selected, metadata, err := models.Posts.SelectMany(ctx, data.PostFilter{
 			PageSize: 1,
-			ID:       uuid.NullUUID{UUID: post.ID, Valid: true},
+			ID:       uuid.NullUUID{UUID: inserted.ID, Valid: true},
 		})
-		assert.NoError(t, err)
-		assert.NotNil(t, selected)
+		require.NoError(t, err)
+		require.NotNil(t, selected)
 		assert.NotEmpty(t, selected)
 		assert.NotEmpty(t, metadata)
 		assert.Equal(t, selected[len(selected)-1].ID, metadata.LastSeen)
 	})
 
 	t.Run("Update", func(t *testing.T) {
+		inserted, err := models.Posts.Insert(ctx, data.PostInput{
+			Title:     "Test",
+			Content:   "Some example content",
+			Published: true,
+		})
+		require.NoError(t, err)
+		require.NotNil(t, inserted)
+
+		t.Cleanup(func() {
+			_, err := models.Posts.Delete(ctx, inserted.ID)
+			require.NoError(t, err)
+		})
+
 		updated, err := models.Posts.Update(
 			ctx,
-			data.PostPatch{ID: post.ID, Deleted: new(true)},
+			data.PostPatch{ID: inserted.ID, Deleted: new(true)},
 		)
-		assert.NoError(t, err)
-		assert.NotNil(t, updated)
-		assert.Equal(t, post.ID, updated.ID)
+		require.NoError(t, err)
+		require.NotNil(t, updated)
+		assert.Equal(t, inserted.ID, updated.ID)
 		assert.NotEmpty(t, updated.DeletedAt)
 		assert.True(t, updated.Deleted)
-
-		post = *updated
 	})
 
 	t.Run("Purge", func(t *testing.T) {
-		deleted, err := models.Posts.Delete(ctx, post.ID)
-		assert.NoError(t, err)
-		assert.NotNil(t, deleted)
-		assert.Equal(t, post.ID, deleted.ID)
-		assert.NotNil(t, deleted.DeletedAt)
-		assert.True(t, deleted.Deleted)
+		inserted, err := models.Posts.Insert(ctx, data.PostInput{
+			Title:     "Test",
+			Content:   "Some example content",
+			Published: true,
+		})
+		require.NoError(t, err)
+		require.NotNil(t, inserted)
+
+		_, err = models.Posts.Delete(ctx, inserted.ID)
+		require.NoError(t, err)
 	})
 }
